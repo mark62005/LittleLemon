a3c81d7a5dcb4b82ff3240abb586a532a0438964
Mark Wong <markwong.digital@gmail.com>
2025-03-07 18:13:27 -0800

    Add seo descriptions for social medias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,18 @@ export const metadata: Metadata = {
 	title: "Little Lemon",
 	description:
 		"Experience the best of Mediterranean cuisine at Little Lemon, a family-owned restaurant blending traditional recipes with a modern twist. Enjoy fresh, flavorful dishes made with love. Reserve your table today!",
+	openGraph: {
+		title: "Little Lemon – Fresh Mediterranean Cuisine with a Modern Twist",
+		description:
+			"At Little Lemon, we serve authentic Mediterranean dishes crafted with fresh ingredients and a touch of modern creativity. Join us for a dining experience full of rich flavors and warm hospitality. Reserve your table today!",
+		siteName: "Little Lemon",
+		images: [
+			{
+				url: "https://example.com/og.png",
+				alt: "Image of Little Lemon's signature dish - Bruschetta",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
