bd39f4b73b2b98d934e7033b8e76b15ff3b29728
Mark Wong <markwong.digital@gmail.com>
2025-03-12 01:34:51 -0700

    Remove excessive components on Home page

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -1,26 +1,15 @@
-import Header from "@/components/Header";
 import HeroSection from "@/components/sections/hero";
 import MenuSection from "@/components/sections/menu";
 import FoodGallerySection from "@/components/sections/food-gallery";
 import ReservationFormSection from "@/components/sections/reservation-form";
-import Footer from "@/components/footer";
-import BackToTopButton from "@/components/BackToTopButton";
 
 export default function Home() {
 	return (
-		<div className="">
-			<Header />
-
-			<main className="">
-				<HeroSection />
-				<MenuSection />
-				<FoodGallerySection />
-				<ReservationFormSection />
-			</main>
-
-			<Footer />
-
-			<BackToTopButton />
-		</div>
+		<main className="min-h-screen">
+			<HeroSection />
+			<MenuSection />
+			<FoodGallerySection />
+			<ReservationFormSection />
+		</main>
 	);
 }
