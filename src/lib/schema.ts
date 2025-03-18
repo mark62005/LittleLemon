f5206bd987a32eb19c7ec1a2bec2c4871d93b47d
Mark Wong <markwong.digital@gmail.com>
2025-03-17 23:21:37 -0700

    Build Reservation Details page for reservation form

---
+++ src/lib/schema.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+/* Reservation Schema */
+export const reservationSchema = z.object({
+	firstName: z.string().min(2, "First name is required"),
+	lastName: z.string().min(2, "Last name is required"),
+	email: z
+		.string()
+		.email(
+			"Invalid email address, please follow the format: example@email.com"
+		),
+	specialRequests: z
+		.string()
+		.max(2500, "This message must not exceed 2500 charaters"),
+});
+
+export type ReservationFormData = z.infer<typeof reservationSchema>;
