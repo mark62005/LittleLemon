e1ce90da7f4291ea80908c29b2b8c5e59071d8a9
Mark Wong <markwong.digital@gmail.com>
2025-03-17 23:22:48 -0700

    Build Reservation Completion page

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,19 +2,39 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface LogoProps {
-	variant?: "default" | "footer";
+	variant?: "default" | "reservation" | "footer";
 }
 function Logo({ variant = "default" }: LogoProps) {
+	if (variant === "reservation") {
+		return (
+			<Link
+				href="/"
+				className="p-2"
+			>
+				<Image
+					src="logo.svg"
+					alt={"Logo of Little Lemon"}
+					width={250}
+					height={44}
+					priority
+				/>
+
+				<h1 className="hidden">Little Lemon</h1>
+			</Link>
+		);
+	}
+
 	return (
 		<Link
 			href="/"
-			className="py-3 pr-2"
+			className="py-2 pr-2"
 		>
 			<Image
 				src="logo.svg"
 				alt={"Logo of Little Lemon"}
 				width={163}
 				height={44}
+				priority
 			/>
 
 			<h1 className="hidden">Little Lemon</h1>
