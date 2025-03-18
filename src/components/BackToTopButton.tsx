2a5890f14147131a8bae9192abc442cb6bfb3027
Mark Wong <markwong.digital@gmail.com>
2025-03-18 00:09:45 -0700

    Finish building back to top button

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { ArrowUp } from "lucide-react";
 
 function BackToTopButton() {
+	function navigateToTop() {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}
+
 	return (
-		<button className="fixed bottom-0 right-0 z-50 bg-zinc-800 p-4 cursor-pointer">
+		<button
+			onClick={navigateToTop}
+			className="fixed bottom-10 right-10 z-50 opacity-80 bg-zinc-600 p-4 rounded-full cursor-pointer hover:bg-zinc-600/70 hover:text-foreground/70"
+		>
 			<ArrowUp className="w-6 h-6" />
 		</button>
 	);
