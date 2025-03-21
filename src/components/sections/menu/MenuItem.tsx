f197f4eb91f46132ac769adc09ba12539d41961a
Mark Wong <markwong.digital@gmail.com>
2025-03-20 20:11:38 -0700

    Create unit test for MenuItem component

--- src/components/sections/menu/MenuItem.tsx
+++ src/components/sections/menu/MenuItem.tsx
@@ -10,11 +10,21 @@ function MenuItem({ dish }: MenuItemProps) {
 	return (
 		<div className="col-span-1">
 			<div className="flex justify-between items-center font-medium">
-				<p className="uppercase">{dishName}</p>
-				<p>{getFormattedPrice(price)}</p>
+				<p
+					className="uppercase"
+					data-testid="dish-name"
+				>
+					{dishName}
+				</p>
+				<p data-testid="dish-price">{getFormattedPrice(price)}</p>
 			</div>
 
-			<p className="w-3/4 font-light">{description}</p>
+			<p
+				className="w-3/4 font-light"
+				data-testid="dish-description"
+			>
+				{description}
+			</p>
 		</div>
 	);
 }
